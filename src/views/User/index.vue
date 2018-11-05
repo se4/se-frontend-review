2b44ffa8138cae6b4fe7d2f17b3506e061c8bba2
Unknown <jinbingshu_nju@qq.com>
2018-11-05 21:36:32 +0800

    save

---
+++ src/views/User/index.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class User extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'bulma/sass/utilities/_all.sass';
+
+</style>
